5145aefa27ced14f3ac681dd2cad8ee4c51f8e87
namph-uet <hoangnamuet.vnu@gmail.com>
2020-07-06 23:47:10 +0700

    update

--- components/screens/GameScreen.js
+++ components/screens/GameScreen.js
@@ -1,10 +1,34 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import styled from "styled-components"
+import { Ionicons } from '@expo/vector-icons'
+import Text from "../Text"
+import gameData from "../../gameData"
+import { StatusBar } from 'expo-status-bar'
 
-export default function GameScreen() {
+export default function GameScreen({route, navigation}) {
+
+    const {game} = route.params
     return (
-        <View>
-            <Text>Game</Text>
-        </View>
+        <GameContainer>
+            <StatusBar barStyle="light-content" />
+
+            <GameArtContainer>
+                <GameArt source={game.cover}/>
+
+                <BackButton onPress={()=> navigation.goBack()}>
+                    <Ionicons name="ios-close" size ={48} color="#fffff"/>
+                </BackButton>
+            </GameArtContainer>
+        </GameContainer>
     )
 }
+
+const GameContainer = styled.View`
+background-color: #343434
+`
+
+const GameArtContainer = styled.View``
+
+const GameArt = styled.Image``
+
+const BackButton = styled.TouchableOpacity``
